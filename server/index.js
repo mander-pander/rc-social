56d4fc858ee98e0f959d8a5cdcb7f93b13f61396
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-06-18 22:07:23 -0700

    tables created, fake users added

--- server/index.js
+++ server/index.js
@@ -9,4 +9,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const {createTables, deleteAllTables, seedData} = require('./controller.js');
+
+//Don't expose this in production
+// app.post('/db', (req, res) => {
+//     createTables(req, res);
+// });
+
+// app.post('/deleteDB', (req, res) => {
+//     deleteAllTables(req, res);
+// });
+
+// app.post('/seedDB', (req, res) => {
+//     seedData(req, res);
+// });
+
 app.listen(SERVER_PORT, () => console.log(`Server running on ${SERVER_PORT}`));
