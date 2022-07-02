e31eb8152d57ff04c0ab744ac86e40855b5fbae4
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-07-02 15:32:22 -0700

    wishlist formatting

--- app/src/Components/Wishlist.js
+++ app/src/Components/Wishlist.js
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Card from 'react-bootstrap/Card';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import Button from 'react-bootstrap/Button';
+import styles from './CSS/DisplayAP.module.css';
 
 export default function Wishlist() {
     const [list, setList] = useState([]);
@@ -13,16 +18,29 @@ export default function Wishlist() {
             },
         })
         .then((res) => {
-            setList(res.data);
+            setList(res.data[0]);
         })
         .catch(err => console.log(err));
-    }, [list]);
+    }, []);
 
     return (
         <div>
+
             {list.map((plane) => {
                 return (
-                    <p>{plane.model}</p>
+                    <div className={styles.planes}>
+                        <Card className={styles.item} style={{ width: '12rem' }} >
+                            <Card.Img varient="top" src={`https://images.unsplash.com/photo-1575116464504-9e7652fddcb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80`} />
+                            <Card.Title>{plane.model}</Card.Title>
+
+                            <OverlayTrigger
+                                placement="bottom"
+                                overlay={<Tooltip id="button-tooltip-2">Remove from wishlist.</Tooltip>}>
+
+                                <Button variant="light" size="sm" onClick={() => console.log('remove')}> Remove </Button>
+                            </OverlayTrigger>
+                        </Card>
+                    </div>
                 )
             })}
         </div>
