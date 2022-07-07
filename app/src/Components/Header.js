76f6697790664f495db88150ad2d0ab7e71506b3
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-07-07 14:47:49 -0700

    added login verification for certain areas of application

--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -1,18 +1,20 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import PostFeed from './PostFeed.js';
 import DisplayAP from './DisplayAP.js';
 import SignUp from './SignUp.js';
 import Login from './Login.js';
 import Wishlist from './Wishlist';
 import DisplayAF from './DisplayAF.js';
-
+import Cookies from 'js-cookie';
 
 export default function Header() {
+    let isLoggedIn = Boolean(Cookies.get('x-access-token'));
+
     return (
         <BrowserRouter>
         <Routes>
-          <Route path="/posts" element={<PostFeed />} />
-          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/posts" element={isLoggedIn ? <PostFeed /> : <Navigate to="/login" />} />
+          <Route path="/wishlist" element={isLoggedIn ? <Wishlist /> : <Navigate to="/login" />} />
           <Route path="/planes" element={<DisplayAP />} />
           <Route path="/createAccount" element={<SignUp/>} />
           <Route path="/login" element={<Login/>} />
